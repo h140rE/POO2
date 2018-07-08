190dfbe40b5af660642286f5860b8c17cedb38c5
GabrielHaddad <gabrielphaddad@yahoo.com.br>
2018-07-08 18:32:09 -0300

    Arrumamos a coisa mais dificil dessa desgraça
    Arrumamos a coisa mais dificil dessa desgraça

--- trabbb/trabalhoPOO2/Controller_MVC/ControllerAtendente.java
+++ trabbb/trabalhoPOO2/Controller_MVC/ControllerAtendente.java
@@ -1,7 +1,40 @@
 package Controller_MVC;
 
+import Model_MVC.Atendente;
 import View_MVC.TelaAtendente;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class ControllerAtendente {
-   
+
+    private Atendente model;
+    private TelaAtendente view;
+
+    public ControllerAtendente(Atendente model, TelaAtendente view) {
+        this.model = model;
+        this.view = view;
+        
+        view.getJButton_MenuPrincipal_Cadastro_Conformar().addActionListener(new CadastraListener());
+        
+    }
+    
+    class CadastraListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String Nome, CPF, Telefone;
+
+            Nome = view.getjTextField1().getText();
+            CPF = view.getjTextField2().getText();
+            Telefone = view.getjTextField3().getText();
+                        
+            model.cadastrar(Nome, CPF, Telefone);
+            
+            
+
+        }
+    }
+
 }
--- trabbb/trabalhoPOO2/Main/TrabalhoPoo.java
+++ trabbb/trabalhoPOO2/Main/TrabalhoPoo.java
@@ -1,23 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
+
 import Model_MVC.*;
 import Controller_MVC.*;
+import View_MVC.*;
 
-/**
- *
- * @author higor
- */
 public class TrabalhoPoo {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        
+
         Animal animal1;
         animal1 = new Cao("Pedro", "lilica", "vira-lata");
         /*
@@ -28,19 +18,26 @@ public class TrabalhoPoo {
         animal1.getEstado().setEstadoEmEstadia();
         animal1.estadoAtual();
         ControllerPet  controller = new ControllerPet();
-       */
-       
-       ContrataServico c = new ContrataServico();
-       Servicos s = new Recibo(animal1);
-       
-       s = c.banho(s, animal1);
-       s = c.consulta(animal1, s);
-       s = c.hotel(s, animal1, 1);
-       s = c.tosa(animal1, s);
-       
-       System.out.println(s.pagar()); 
-       System.out.println(s.getDescricao());   
-       
+         */
+
+        ContrataServico c = new ContrataServico();
+        Servicos s = new Recibo();
+
+        s = c.banho(s);
+        s = c.consulta(animal1, s);
+        s = c.hotel(s, 1);
+        s = c.tosa(animal1, s);
+
+        System.out.println(s.pagar());
+        System.out.println(s.getDescricao());
+
+        TelaAtendente a = new TelaAtendente();
+        a.setVisible(true);
+        
+        Atendente a2 = new Atendente();
+
+        ControllerAtendente cA = new ControllerAtendente(a2,a);
+
     }
-    
+
 }
