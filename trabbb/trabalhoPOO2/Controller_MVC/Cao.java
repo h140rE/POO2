e756cdd595015476a0a4810fcacd77592e08192b
João Paulo <joaopaulodeoliveira123@gmail.com>
2018-07-08 11:34:47 -0300

    Arrumei o Model e Controller

--- trabbb/trabalhoPOO2/Controller_MVC/Cao.java
+++
@@ -1,9 +0,0 @@
-package Controller_MVC;
-
-public class Cao extends Animal {
-
-    public Cao(String nomeDono, String nomeAnimal, String raca) {
-        super(nomeDono, nomeAnimal, raca);
-    }
-
-}
