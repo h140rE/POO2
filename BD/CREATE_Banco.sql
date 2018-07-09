bf37858c958adbe77ac0c96d46ff3016ac9cec8e
João Paulo <joaopaulodeoliveira123@gmail.com>
2018-07-09 08:52:54 -0300

    O restante dos inserts, vai ser tirado do programa

--- BD/CREATE_Banco.sql
+++ BD/CREATE_Banco.sql
@@ -52,7 +52,7 @@ CREATE TABLE Cliente(
 
 CREATE TABLE Animal(
 	cpfDono		varchar(11),
---Cão ou gato
+        --Cão ou gato
         tipo            char,
 	nome		varchar(30),
 	raca            varchar(30),
@@ -68,7 +68,7 @@ CREATE TABLE Recibo(
         idCompra        serial,
         cpf		varchar(11),
 	preco           float,
-	Descricao       varchar(400),
+	Descricao       varchar(400) NOT NULL,
 	CONSTRAINT recibo_pk PRIMARY KEY (idCompra),
 	CONSTRAINT donoRecibo_fk FOREIGN KEY (cpf) REFERENCES cliente(cpf)
 );
@@ -83,70 +83,67 @@ CREATE TABLE ReciboProduto(
 
 --Inserts
 
-		--CORRIGIR OS TELEFONES PARA VARCHAR
 INSERT INTO Atendente (nome,cpf,telefone,id) VALUES
-        ('Hedley',      '40049008080',4766685,nextval('funcionarioID')),
-        ('Rae',         '99888670000',813671,nextval('funcionarioID')),
-        ('Chantale',    '28464557086',107870,nextval('funcionarioID')),
-        ('Rooney',      '48996240001',5861599,nextval('funcionarioID')),
-        ('Alana',       '75383899030',6786126,nextval('funcionarioID')),
-        ('Deacon',      '99884168067',3872661,nextval('funcionarioID')),
-        ('Ahmed',       '04870533030',7532681,nextval('funcionarioID')),
-        ('Lacota',      '52705686088',174247,nextval('funcionarioID')),
-        ('Armando',     '93994973058',4169352,nextval('funcionarioID')),
-        ('Germane',     '77751509040',9180391,nextval('funcionarioID'));
+        ('Hedley',      '40049008080','033993604670',nextval('funcionarioID')),
+        ('Rae',         '99888670000','031986811808',nextval('funcionarioID')),
+        ('Chantale',    '28464557086','109483727543',nextval('funcionarioID')),
+        ('Rooney',      '48996240001','103547273940',nextval('funcionarioID')),
+        ('Alana',       '75383899030','123476786126',nextval('funcionarioID')),
+        ('Deacon',      '99884168067','387266121312',nextval('funcionarioID')),
+        ('Ahmed',       '04870533030','753268134235',nextval('funcionarioID')),
+        ('Lacota',      '52705686088','174247123545',nextval('funcionarioID')),
+        ('Armando',     '93994973058','416935223467',nextval('funcionarioID')),
+        ('Germane',     '77751509040','918039123406',nextval('funcionarioID'));
         
 INSERT INTO Cuidador (nome,cpf,telefone,id) VALUES
-        ('Rahim',       '19023600010',1496504,nextval('funcionarioID')),
-        ('Kermit',      '05383374000',6429076,nextval('funcionarioID')),
-        ('Luke',        '45881915003',794928,nextval('funcionarioID')),
-        ('Cadman',      '73066573052',5643168,nextval('funcionarioID')),
-        ('Cullen',      '90216865034',6910032,nextval('funcionarioID')),
-        ('Lacey',       '42482082021',1588803,nextval('funcionarioID')),
-        ('Byron',       '80723081069',1927255,nextval('funcionarioID')),
-        ('Unity',       '41272499006',6898561,nextval('funcionarioID')),
-        ('Veronica',    '64527142011',2528650,nextval('funcionarioID')),
-        ('Angela',      '42299586091',5197859,nextval('funcionarioID'));
+        ('Rahim',       '19023600010','149650423467',nextval('funcionarioID')),
+        ('Kermit',      '05383374000','642907612456',nextval('funcionarioID')),
+        ('Luke',        '45881915003','794928124456',nextval('funcionarioID')),
+        ('Cadman',      '73066573052','564316823456',nextval('funcionarioID')),
+        ('Cullen',      '90216865034','691003209843',nextval('funcionarioID')),
+        ('Lacey',       '42482082021','158880334985',nextval('funcionarioID')),
+        ('Byron',       '80723081069','192725523581',nextval('funcionarioID')),
+        ('Unity',       '41272499006','689856123356',nextval('funcionarioID')),
+        ('Veronica',    '64527142011','252865023608',nextval('funcionarioID')),
+        ('Angela',      '42299586091','124324519785',nextval('funcionarioID'));
 
 INSERT INTO Veterinario (nome,cpf,telefone,id) VALUES 
-        ('Jescie',      '93326327072',3091173,nextval('funcionarioID')),
-        ('Brenda',      '27542004018',7702155,nextval('funcionarioID')),
-        ('Erich',       '97094094094',7912074,nextval('funcionarioID')),
-        ('Dolan',       '79614790075',4344647,nextval('funcionarioID')),
-        ('Zenia',       '68023779001',3305629,nextval('funcionarioID')),
-        ('Keane',       '05197371056',8375877,nextval('funcionarioID')),
-        ('Alexander',   '96598398045',7720974,nextval('funcionarioID')),
-        ('Samantha',    '99917701001',4250078,nextval('funcionarioID')),
-        ('Joel',        '69440133040',6130952,nextval('funcionarioID')),
-        ('Lewis',       '43666598080',2144230,nextval('funcionarioID'));
+        ('Jescie',      '93326327072','309117323445',nextval('funcionarioID')),
+        ('Brenda',      '27542004018','770215532568',nextval('funcionarioID')),
+        ('Erich',       '97094094094','791207444469',nextval('funcionarioID')),
+        ('Dolan',       '79614790075','434464745647',nextval('funcionarioID')),
+        ('Zenia',       '68023779001','330562934928',nextval('funcionarioID')),
+        ('Keane',       '05197371056','837587732400',nextval('funcionarioID')),
+        ('Alexander',   '96598398045','772097412357',nextval('funcionarioID')),
+        ('Samantha',    '99917701001','425007823457',nextval('funcionarioID')),
+        ('Joel',        '69440133040','613095223506',nextval('funcionarioID')),
+        ('Lewis',       '43666598080','122144230534',nextval('funcionarioID'));
 
---CORRIGIR O NOME DOS PRODUTOS
 INSERT INTO Produtos (codBarras,preco,nome,quantidade) VALUES
-	(2,5.0   ,'Felicia',52),
-	(47,97.0 ,'Uta',80),
-	(87,57.0 ,'Quincy',28),
-	(28,23.0 ,'Tamara',16),
-	(72,9.0  ,'Kylan',86),
-	(50,25.0 ,'Blythe',5),
-	(55,92.0 ,'Christen',77),
-	(84,8.0  ,'Brittany',6),
-	(60,58.0 ,'Jael',37),
-	(53,31.0 ,'Veda',9);
+	(2,5.0   ,'Shampoo Haskell',52),
+	(47,97.0 ,'Condicionador Eudora',80),
+	(87,57.0 ,'Escova Dogzeira',28),
+	(28,23.0 ,'Gravatinha',16),
+	(72,9.0  ,'Pato de plástico Borracheiro',86),
+	(50,25.0 ,'Vermífago Irineu',5),
+	(55,92.0 ,'Caixa de areia Depósito',77),
+	(84,8.0  ,'Ração DeVaca',6),
+	(60,58.0 ,'Ração Magnum',37),
+	(53,31.0 ,'Apito DoKralho',9);
 
 INSERT INTO Hotel VALUES (5);
 
---CORRIGIR TELEFONES
 INSERT INTO cliente (nome,cpf,telefone) VALUES 
-        ('Myra',        '03834902004',539972316474),
-        ('Ruby',        '33672413003',497057146858),
-	('Ivor',        '17278598044',242117166519),
-	('Harlan',      '01143237048',586993906181),
-	('Belle',       '36226158000',965466029476),
-	('Kimberley',   '85390972066',767956910655),
-	('Coby',        '51911345087',977018760982),
-	('Virginia',    '34976352005',185882407241),
-	('Yardley',     '19383455098',956941482611),
-	('Callum',      '84592076095',906977694016);
+        ('Myra',        '03834902004','539972316474'),
+        ('Ruby',        '33672413003','497057146858'),
+	('Ivor',        '17278598044','242117166519'),
+	('Harlan',      '01143237048','586993906181'),
+	('Belle',       '36226158000','965466029476'),
+	('Kimberley',   '85390972066','767956910655'),
+	('Coby',        '51911345087','977018760982'),
+	('Virginia',    '34976352005','185882407241'),
+	('Yardley',     '19383455098','956941482611'),
+	('Callum',      '84592076095','906977694016');
 
 INSERT INTO Animal (cpfDono, tipo, nome, raca, estado) VALUES
         ('84592076095', 'c', 'Duque', 	'Bulldog',		'Finalizado'),
@@ -167,7 +164,17 @@ INSERT INTO Animal (cpfDono, tipo, nome, raca, estado) VALUES
 
 
 
---INSERT INTO recibo (idCompra,cpf,preco, descricao) VALUES 
+INSERT INTO recibo (idCompra,cpf,preco, descricao) VALUES 
+        ( nextval(idCompra), '84592076095', , ),
+        ( nextval(idCompra), '19383455098', , ),
+        ( nextval(idCompra), '03834902004', , ),
+        ( nextval(idCompra), '03834902004', , ),
+        ( nextval(idCompra), '34976352005', , ),
+        ( nextval(idCompra), '34976352005', , ),
+        ( nextval(idCompra), '85390972066', , ),
+        ( nextval(idCompra), '85390972066', , ),
+        ( nextval(idCompra), '34976352005', , ),
+        ( nextval(idCompra), '34976352005', , );
  
  
 --INSERT INTO reciboproduto (nome,cpf,telefone) VALUES 
