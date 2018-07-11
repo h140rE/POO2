91f9b0846c370e5a24f9648f5a2a50b1984acab7
matheuspr96 <matheuspr96@hotmail.com>
2018-07-11 20:47:17 -0300

    Haddad push Controller atendente

--- trabbb/trabalhoPOO2/Controller_MVC/ControllerAtendente.java
+++ trabbb/trabalhoPOO2/Controller_MVC/ControllerAtendente.java
@@ -8,7 +8,10 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
 public class ControllerAtendente {
@@ -24,9 +27,10 @@ public class ControllerAtendente {
     CadastraCliente cadastraC;
     MarcaConsulta marcaC;
     private LinkedList<Cliente> clientesAtivos;
+    SQL testesql;
 
     public ControllerAtendente(JanelaPrincipal jPrincipal, Atendente model, TelaAtendente view, CadastraCliente cadastraC,
-            BuscaCliente buscaCliente, CadastraAnimal cadastraA, MarcaConsulta marcaC, LinkedList<Cliente> cliente) {
+            BuscaCliente buscaCliente, CadastraAnimal cadastraA, MarcaConsulta marcaC, LinkedList<Cliente> cliente, SQL testesql) {
 
         this.jPrincipal = jPrincipal;
         this.atendente = model;
@@ -36,7 +40,7 @@ public class ControllerAtendente {
         this.cadastraA = cadastraA;
         this.marcaC = marcaC;
         this.clientesAtivos = cliente;
-        
+        this.testesql = testesql;
 
         this.view.getMenuCadastraCliente().addActionListener(new CadastraClienteJanelaListener());
         this.view.getMenuBuscaCliente().addActionListener(new BuscaClienteJanelaListener());
@@ -122,12 +126,17 @@ public class ControllerAtendente {
             if (Nome.equals("") || CPF.equals("") || Telefone.equals("")) {
                 JOptionPane.showMessageDialog(null, "Preencha todos os campos");
             } else {
-                clientesAtivos.add(atendente.atende(Nome, CPF, Telefone));
-
-                limpaBufferCadastra();
-                adicionaTabela(clientesAtivos);
+                try {
+                    testesql.insereCliente(Nome, CPF, Telefone);
+                    clientesAtivos.add(atendente.atende(Nome, CPF, Telefone));
+                    limpaBufferCadastra();
+                    adicionaTabela(clientesAtivos);
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(ControllerAtendente.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerAtendente.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
-
         }
     }
 
@@ -137,7 +146,6 @@ public class ControllerAtendente {
         public void actionPerformed(ActionEvent e) {
             limpaBufferCadastra();
         }
-
     }
 
     private void adicionaTabela(LinkedList<Cliente> clientesAtivos) {
@@ -163,25 +171,6 @@ public class ControllerAtendente {
         public void actionPerformed(ActionEvent e) {
             limpaBufferBusca();
         }
-
-    }
-
-    class ConfirmaBuscaListener implements ActionListener {
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-            String NomeCliente = buscaCliente.getjTextField1().getText();
-
-            if (NomeCliente.equals("")) {
-                JOptionPane.showMessageDialog(null, "Preencha todos os campos!!!");
-            } else {
-                JOptionPane.showMessageDialog(null, "Não implementado!!!");
-
-            }
-
-        }
-
     }
 
     class BotaoBuscaListener implements ActionListener {
@@ -189,12 +178,31 @@ public class ControllerAtendente {
         @Override
         public void actionPerformed(ActionEvent e) {
 
-            String NomeCliente = buscaCliente.getjTextField1().getText();
+            String CPF = buscaCliente.getjTextField1().getText();
 
-            if (NomeCliente.equals("")) {
+            if (CPF.equals("")) {
                 JOptionPane.showMessageDialog(null, "Preencha todos os campos!!!");
             } else {
-                JOptionPane.showMessageDialog(null, "Não implementado!!!");
+                try {
+                    Cliente c = testesql.getClienteBanco(CPF);
+                    if (c == null) {
+                        JOptionPane.showMessageDialog(null, "Cliente não encontrado!!!");
+                    }
+                    buscaCliente.getjTextField2().setText(c.getNome());
+                    buscaCliente.getjTextField3().setText(c.getTelefone());
+
+                    clientesAtivos.add(atendente.atende(buscaCliente.getjTextField1().getText(), buscaCliente.getjTextField2().getText(),
+                            buscaCliente.getjTextField3().getText()));
+
+                    limpaBufferBusca();
+
+                    adicionaTabela(clientesAtivos);
+
+                } catch (ClassNotFoundException ex) {
+                    Logger.getLogger(ControllerAtendente.class.getName()).log(Level.SEVERE, null, ex);
+                } catch (SQLException ex) {
+                    Logger.getLogger(ControllerAtendente.class.getName()).log(Level.SEVERE, null, ex);
+                }
 
             }
         }
