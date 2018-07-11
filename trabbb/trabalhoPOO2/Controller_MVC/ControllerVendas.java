7cb9c7e47683f5a30f90ed5afc5bbd5531f9a504
GabrielHaddad <gabrielphaddad@yahoo.com.br>
2018-07-11 16:20:54 -0300

    wdqw
    qwdqw

--- trabbb/trabalhoPOO2/Controller_MVC/ControllerVendas.java
+++ trabbb/trabalhoPOO2/Controller_MVC/ControllerVendas.java
@@ -1,65 +1,68 @@
 package Controller_MVC;
 
-import Model_MVC.Recibo;
-import Model_MVC.Servicos;
-import View_MVC.CadastraCliente;
-import View_MVC.JanelaPrincipal;
-import View_MVC.CadastraCliente;
-import View_MVC.TelaHBT;
-import View_MVC.TelaInicial;
+import Model_MVC.Cuidador;
 import View_MVC.TelaVendaServico;
 import View_MVC.TelaVendas;
 import View_MVC.TelaVendasProdutos;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.JOptionPane;
 
 public class ControllerVendas {
-    
+
     private TelaVendas view;
-    private TelaVendasProdutos viewProdutos;
-    private TelaHBT viewBanho;
+    private Cuidador model;
     private TelaVendaServico viewServico;
-    private ControllerVendaProdutos controllerProdutos;
-    private ControllerHBT controllerBanho;
-    private ControllerVendaServico controllerServico;
-    
-    CadastraCliente cad = new CadastraCliente();
-    
-    JanelaPrincipal viewJPrincipal;
-    TelaInicial viewTelaInicial;
-   
-   
-    
-    public ControllerVendas(CadastraCliente  cad, JanelaPrincipal viewJPrincipal) {
-        this.viewJPrincipal = viewJPrincipal;
-         view = new TelaVendas();
-         view.getjButton_Produtos().addActionListener(new AbreProdutosListener());
-      
+    private TelaVendasProdutos viewProdutos;
+
+    public ControllerVendas(TelaVendas view, Cuidador model, TelaVendaServico viewServico, TelaVendasProdutos viewProdutos) {
+        this.view = view;
+        this.model = model;
+        this.viewProdutos = viewProdutos;
+        this.viewServico = viewServico;
+        view.setSize(960, 500);
+
+        view.getjButton_Produtos().addActionListener(new AbreProdutosListener());
+        view.getjButton_BuscaCliente().addActionListener(new VerificaListener());
+        view.getjButton_Servicos().addActionListener(new AbreServicosListener());
+
     }
-    
+
     class AbreProdutosListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("rola");
+
+            JOptionPane.showMessageDialog(null, "Não Implementado!!!");
+
         }
     }
-    
+
+    class VerificaListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+            String CPF;
+
+            CPF = view.getjTextPane3().getText();
+
+            if (CPF.equals("")) {
+                JOptionPane.showMessageDialog(null, "Preencha todos os campos!!");
+            } else {
+                JOptionPane.showMessageDialog(null, "Não Implementado!!!");
+            }
+        }
+    }
+
     class AbreServicosListener implements ActionListener {
 
         @Override
         public void actionPerformed(ActionEvent e) {
 
-           viewServico = new TelaVendaServico();
-           viewServico.setVisible(true);
-           controllerServico = new ControllerVendaServico(viewServico);
-            
+            JOptionPane.showMessageDialog(null, "Não Implementado!!!");
+
         }
     }
-    
-    
-    
-    
+
 }
-
-
